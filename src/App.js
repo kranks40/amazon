c74a6d390ad88126e781d72a41852c19850fbc42
kranks40 <eng_peters20@yahoo.com>
2020-09-13 20:17:45 -0400

    Header and nav bar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     //BEM convention
     <div className="app">
-     <h1>Hello this is Amazon Clone!</h1>
+     <Header/>
+     <Home/>
+
+     {/* Header */}
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+
+
+function Header() {
+    return (
+        <div className='header'>
+            <MenuOutlinedIcon className='header__menu'/>
+            <img className='header__logo'
+            src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'alt=''/>
+
+            <div className="header__search">
+                <input className='header__searchInput' type='text' placeholder='Search Amazon' aria-label data-aria-clear-label='Clear search keywords' name='k' autoComplete='off' autoCorrect='off' autoCapitalize='off' dir='auto'/>
+                <SearchIcon className='header__searchIcon'/>
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionOne">Hello Oketo</span>
+                    <span className="header__optionTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionOne">Returns</span>
+                    <span className="header__optionTwo">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionOne">Your</span>
+                    <span className="header__optionTwo">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingCartOutlinedIcon/>
+                    <span className="header__optionTwo header__basketCount">0</span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
