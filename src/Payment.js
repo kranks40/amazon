4c992e4c2b174a3a953b337c9d76565fbb339562
kranks40 <eng_peters20@yahoo.com>
2020-09-19 19:08:55 -0400

    Made some changes to payment component

--- src/Payment.js
+++ src/Payment.js
@@ -10,6 +10,7 @@ import { getBasketTotal } from './reducer';
 import axios from './axios';
 import { db } from './firebase';
 
+
 function Payment() {
     const [{ basket, user}, dispatch] = useStateValue();
     const history = useHistory();
@@ -23,7 +24,7 @@ function Payment() {
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(true);
 
-    const [clientSecret, setClienSecret] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
 
     useEffect(() => {
         // generate that special stripe secret which allow us to charge the customer
@@ -31,9 +32,9 @@ function Payment() {
             const response = await axios({
                 method: 'post',
                 // Stripe expects the total in a currencies subunits
-                url:`/payments/create?total=${getBasketTotal(basket) * 100}`
+                url: `/payments/create?total=${getBasketTotal(basket) * 100}`
             });
-            setClienSecret(response.data.clientSecret)
+            setClientSecret(response.data.clientSecret)
         }
         getClientSecret();
     }, [basket])
@@ -49,7 +50,12 @@ function Payment() {
         }).then(({ paymentIntent}) => {
             //paymentIntent = payment confirmation
 
-            db.collection('users').doc(user?.uid).collection('orders').doc(paymentIntent.id).set({
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .doc(paymentIntent.id)
+            .set({
                 basket: basket,
                 amount: paymentIntent.amount,
                 created: paymentIntent.created
@@ -121,20 +127,19 @@ function Payment() {
                                 <div className="payment__priceContainer">
                                     <CurrencyFormat
                                     renderText={(value) => (
-                                        <>
                                         <h3>Order Total: {value}</h3>
-                                        </>
                                     )}
                                     decimalScale={2}
-                                    value={getBasketTotal()}
+                                    value={getBasketTotal(basket)}
                                     displayType={'text'}
+                                    thousandSepaator
                                     prefix={'$'}
                                     />
-                                    <button disabled={processing || disabled || succeeded }>
-                                        <span>{processing ? <p>Processing</p> : 'Buy Now'}</span>
+                                   <button disabled={processing || disabled || succeeded}>
+                                        <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
                                     </button>
                                 </div>
-                                
+                                {error && <div>{error}</div>}
                             </form>
 
                         </div>
@@ -145,4 +150,4 @@ function Payment() {
     )
 }
 
-export default Payment
+export default Payment;
